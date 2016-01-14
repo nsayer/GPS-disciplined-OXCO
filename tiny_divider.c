d661a47254c1283d2879402feceb9fdb699f8ae7
Nick Sayer <nsayer@kfu.com>
2016-01-13 21:27:18 -0800

    Switch to using timer1. It’s a little bit more complicated to use the way we want, but there are more prescale options. We need to use OCR1A and OCR1C together - OCR1A will trigger the interrupt and OCR1C will clear TCNT1. We can’t have just one register do both, it seems.

--- tiny_divider.c
+++ tiny_divider.c
@@ -28,10 +28,10 @@
  * means that you have to clock it during programming).
  * low = 0xE0, high = 0xDF, extended = 0xff
  *
- * The OC0A pin will toggle with the given divide ratio.
+ * The OC1A pin will toggle with the given divide ratio.
  *
  * Pin 1 is RESET, pin 2 is the input clock, pin 4 is ground,
- * pin 5 is the output clock, pin 7 is Vcc. Pins 3 and 6 are NC.
+ * pin 6 is the output clock, pin 7 is Vcc. Pins 3 and 5 are NC.
  */
 
 #include <avr/io.h>
@@ -40,28 +40,32 @@
 #include <avr/interrupt.h>
 
 // Set this to the divisor - we will toggle at double this divisor.
-#define DIVISOR 10000000L
+#define DIVISOR (10000000L)
+
+#define PRESCALE (32)
 
 // our division cycling consists of cycle_count full 256 count
 // interrupts, followed by a single "short" cycle consisting of
 // last_cycle counts. At the end of the "short" cycle, we
-// toggle OC0A.
+// toggle OC1A.
 volatile unsigned int cycle_number;
 volatile unsigned int cycle_count;
 volatile unsigned char last_cycle;
 
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER1_COMPA_vect) {
   cycle_number++;
   if (cycle_number == cycle_count - 1) {
     // This is the last cycle. Set up the timer
     // with the short length for the *next* cycle,
-    OCR0A = last_cycle - 1; // it's zero-based *and* inclusive.
-  } else if (cycle_number == cycle_count) {
+    OCR1C = last_cycle - 1;
+    OCR1A = last_cycle - 1;
+  } else if (cycle_number == cycle_count - 0) {
     // This is the short cycle. Do the toggle
     // at the end of it, and set up to go back
     // to long cycles.
-    OCR0A = 0xff; // go back to long ones after this
-    TCCR0A ^= _BV(COM0A0); // toggle OC0A match state
+    OCR1C = 0xff; // go back to long ones after this
+    OCR1A = 0xff; // go back to long ones after this
+    TCCR1 ^= _BV(COM1A0); // toggle OC1A match state
   } else if (cycle_number == cycle_count + 1) {
     // This is the "zero-eth" cycle.
     cycle_number = 0; // restart
@@ -73,22 +77,23 @@ void main() {
   ACSR = _BV(ACD); // Turn off analog comparator - but was it ever on anyway?
   power_adc_disable();
   power_usi_disable();
-  power_timer1_disable();
+  power_timer0_disable();
   DDRB = _BV(DDB0) | _BV(DDB1) | _BV(DDB2); // all our pins are output.
-  TCCR0A = _BV(COM0A1) | _BV(WGM01); // mode 2 - CTC. Clear OC0A on match
-  TIMSK = _BV(OCIE0A); // turn on output compare A interrupt
-  TCCR0B = _BV(CS00) | _BV(CS01); // prescale = 64
-  OCR0A = 0xff; // start with the timer maxed out.
+  // Keep the CS1x bits in sync with the value of PRESCALE above!!
+  TCCR1 = _BV(CTC1) | _BV(COM1A1) | _BV(CS12) | _BV(CS11); // CTC. Clear OC1A on match. Prescale = 32
+  TIMSK = _BV(OCIE1A); // turn on compare interrupt
+  OCR1C = 0xff; // start with the timer maxed out.
+  OCR1A = 0xff; // start with the timer maxed out.
   PORTB = 0; // Initialize all pins low.
 
   // Do the math. Divide the DIVISOR by 2 to make up for the fact
-  // that it's a toggling rate. Divide by 64 for the prescaler
+  // that it's a toggling rate. Divide by the prescaler
   // and then divide by 256 to get the quotient, which is the
   // number of full 256 count cycles.
-  cycle_count = (unsigned int)(DIVISOR / (2L * 64 * 256));
+  cycle_count = (unsigned int)(DIVISOR / (2L * PRESCALE * 256));
   // The remainder after the above division is the number
   // of counts in the *short* cycle.
-  last_cycle = (unsigned char)((DIVISOR / (2 * 64)) - (cycle_count * 256));
+  last_cycle = (unsigned char)((DIVISOR / (2 * PRESCALE)) - (cycle_count * 256));
 
   sei(); // turn on interrupts
   while(1); // And we're done.
